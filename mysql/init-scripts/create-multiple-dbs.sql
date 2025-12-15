24f3d6c2a926d97077521ab6580167f1fa126178
moyisuosheng <moyisuosheng@outlook.com>
2025-12-15 21:30:04 +0800

    ✨ feat(sentinel): 添加sentinel部署脚本。

--- mysql/init-scripts/create-multiple-dbs.sql
+++ mysql/init-scripts/create-multiple-dbs.sql
@@ -1,60 +1,79 @@
--- 初始化脚本会在 MYSQL_DATABASE 指定的库创建后执行。
+-- 初始化脚本会在 mysql_database 指定的库创建后执行。
 -- 此处创建额外的数据库
 create schema if not exists `rm-system`;
 create schema if not exists `rm-file`;
 create schema if not exists `rm-search`;
 create schema if not exists `seata`;
 -- 你也可以为不同的数据库创建用户并授权（可选）
--- CREATE USER 'analytics_user'@'%' IDENTIFIED BY 'user_password';
--- GRANT ALL PRIVILEGES ON `app_analytics`.* TO 'analytics_user'@'%';
+-- create user 'analytics_user'@'%' identified by 'user_password';
+-- grant all privileges on `app_analytics`.* to 'analytics_user'@'%';
 
-CREATE TABLE if not exists `rm-file`.`file`
+create table if not exists `rm-file`.resource_file
 (
-    `id`              bigint NOT NULL COMMENT '主键ID',
-    `update_time`     DATETIME     DEFAULT NULL COMMENT '更新时间戳',
-    `update_user`     varchar(64)  DEFAULT NULL COMMENT '更新人',
-    `create_time`     DATETIME     DEFAULT NULL COMMENT '创建时间戳',
-    `create_user`     varchar(64)  DEFAULT NULL COMMENT '创建人',
-    `deleted`         bigint       DEFAULT 0 COMMENT '逻辑删除标识（0=未删除，值=id时=已删除）',
-    `file_name`       varchar(255) DEFAULT NULL COMMENT '文件名',
-    `file_url`        varchar(512) DEFAULT NULL COMMENT '文件地址',
-    `file_identifier` varchar(128) DEFAULT NULL COMMENT '文件标识符',
-    `file_type`       varchar(64)  DEFAULT NULL COMMENT '文件类型',
-    `status`          tinyint      DEFAULT 1 COMMENT '状态（0=禁用；1=正常）',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4 COMMENT ='文件信息表';
+    id               bigint                             not null comment '主键'
+        primary key,
+    record_id        bigint                             not null comment '资源文件记录id',
+    upload_id        varchar(255)                       not null comment 'minio内文件上传标识',
+    summary          varchar(255)                       not null comment '文件摘要',
+    summary_type     varchar(64)                        not null comment '文件摘要类型',
+    bucket           varchar(64)                        not null comment '存储桶',
+    object_key       varchar(255)                       not null comment 'minio内文件名',
+    origin_file_name varchar(255)                       not null comment '原始文件名',
+    size             bigint                             not null comment '文件大小（字节）',
+    content_type     varchar(512)                       not null comment '文件类型',
+    chunk_size       bigint                             not null comment '分片大小（字节）',
+    chunk_count      int                                not null comment '分片数量',
+    tenant           varchar(128)                       not null comment '租户',
+    update_time      datetime default current_timestamp not null on update current_timestamp comment '更新时间戳',
+    update_user      varchar(64)                        null comment '更新人',
+    create_time      datetime default current_timestamp not null comment '创建时间戳',
+    create_user      varchar(64)                        null comment '创建人',
+    deleted          bigint   default 0                 null comment '逻辑删除标识（0=未删除，值=id时=已删除）'
+)
+    comment '文件表' row_format = dynamic;
 
-CREATE TABLE if not exists `rm-file`.`tag`
+create table if not exists `rm-file`.resource_file_record
 (
-    `id`          bigint NOT NULL COMMENT '主键ID',
-    `update_time` DATETIME    DEFAULT NULL COMMENT '更新时间戳',
-    `update_user` varchar(64) DEFAULT NULL COMMENT '更新人',
-    `create_time` DATETIME    DEFAULT NULL COMMENT '创建时间戳',
-    `create_user` varchar(64) DEFAULT NULL COMMENT '创建人',
-    `deleted`     bigint      DEFAULT 0 COMMENT '逻辑删除标识（0=未删除，值=id时=已删除）',
-    `tag_name`    varchar(64) DEFAULT NULL COMMENT '标签名称',
-    `status`      tinyint     DEFAULT 1 COMMENT '状态（0=禁用；1=正常）',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4 COMMENT ='标签信息表';
+    id               bigint                             not null comment '主键'
+        primary key,
+    upload_id        varchar(255)                       not null comment 'minio内文件上传标识',
+    summary          varchar(255)                       not null comment '文件摘要',
+    summary_type     varchar(64)                        not null comment '文件摘要类型',
+    slice            tinyint                            not null comment '是否切片（切片上传：true，非切片上传：false）',
+    bucket           varchar(64)                        not null comment '存储桶',
+    object_key       varchar(255)                       not null comment 'minio内文件名',
+    origin_file_name varchar(255)                       not null comment '原始文件名',
+    size             bigint                             not null comment '文件大小（字节）',
+    content_type     varchar(512)                       not null comment '文件类型',
+    chunk_size       bigint                             not null comment '分片大小（字节）',
+    chunk_count      int                                not null comment '分片数量',
+    tenant           varchar(128)                       not null comment '租户',
+    update_time      datetime default current_timestamp not null on update current_timestamp comment '更新时间戳',
+    update_user      varchar(64)                        null comment '更新人',
+    create_time      datetime default current_timestamp not null comment '创建时间戳',
+    create_user      varchar(64)                        null comment '创建人',
+    deleted          bigint   default 0                 null comment '逻辑删除标识（0=未删除，值=id时=已删除）'
+)
+    comment '文件记录表' row_format = dynamic;
 
-CREATE TABLE if not exists `rm-system`.`user`
+create table if not exists `rm-file`.resource_file_slice_record
 (
-    `id`          bigint NOT NULL COMMENT '主键ID',
-    `update_time` DATETIME     DEFAULT NULL COMMENT '更新时间戳',
-    `update_user` varchar(64)  DEFAULT NULL COMMENT '更新人',
-    `create_time` DATETIME     DEFAULT NULL COMMENT '创建时间戳',
-    `create_user` varchar(64)  DEFAULT NULL COMMENT '创建人',
-    `deleted`     bigint       DEFAULT 0 COMMENT '逻辑删除标识（0=未删除，值=id时=已删除）',
-    `name`        varchar(64)  DEFAULT NULL COMMENT '姓名',
-    `nickname`    varchar(64)  DEFAULT NULL COMMENT '昵称',
-    `phone`       varchar(20)  DEFAULT NULL COMMENT '手机号',
-    `password`    varchar(128) DEFAULT NULL COMMENT '密码，加密存储',
-    `gender`      varchar(10)  DEFAULT NULL COMMENT '性别',
-    `id_number`   varchar(32)  DEFAULT NULL COMMENT '身份证号',
-    `avatar`      varchar(512) DEFAULT NULL COMMENT '头像地址',
-    `status`      tinyint      DEFAULT 1 COMMENT '状态（0=禁用；1=正常）',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4 COMMENT ='用户信息表';
+    id              bigint                             not null comment '主键'
+        primary key,
+    record_id       bigint                             not null comment '资源文件记录id',
+    part_number     int                                not null comment '切片编号',
+    summary         varchar(255)                       not null comment '切片文件摘要',
+    summary_type    varchar(64)                        not null comment '切片文件摘要类型',
+    url             text                               not null comment '切片上传地址',
+    expiration_time datetime                           not null comment '过期时间',
+    tenant          varchar(128)                       not null comment '租户',
+    update_time     datetime default current_timestamp not null on update current_timestamp comment '更新时间戳',
+    update_user     varchar(64)                        null comment '更新人',
+    create_time     datetime default current_timestamp not null comment '创建时间戳',
+    create_user     varchar(64)                        null comment '创建人',
+    deleted         bigint   default 0                 null comment '逻辑删除标识（0=未删除，值=id时=已删除）'
+)
+    comment '文件切片记录表' row_format = dynamic;
+
+
+
